c09441fb9a655980528f69137163a67c1cb34239
tadeu jorge <tadeujorgetls@gmail.com>
2025-10-17 21:51:57 -0300

    Ajuste estetico no icone de soma e mediana

--- app/constants/calculation.constants.ts
+++ app/constants/calculation.constants.ts
@@ -1,9 +1,9 @@
 import { CalcType } from "../types/calculation.types";
 
 export const CALC_TYPES: CalcType[] = [
-  { value: "soma", label: "Soma", icon: "∑" },
+  { value: "soma", label: "Soma", icon: "+" },
   { value: "media", label: "Média", icon: "μ" },
-  { value: "mediana", label: "Mediana", icon: "M" },
+  { value: "mediana", label: "Mediana", icon: "Md" },
   { value: "moda", label: "Moda", icon: "Mo" },
   { value: "maior_numero", label: "Maior", icon: "↑" },
   { value: "menor_numero", label: "Menor", icon: "↓" },
